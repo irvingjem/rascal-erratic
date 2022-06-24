a90ff6e9b6cce771c7bc94e68a9ab49edd3a6876
Your Name <irving.jem@gmail.com>
2022-06-24 08:59:07 -0700

    added basic login logic

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+
+// Apollo Server import Typedefs and resolvers
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require ("./schema"); 
+
+// Middleware
+const { authMiddleware } = require('./utils/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+async function startApolloServer(){
+await server.start();
+server.applyMiddleware({ app });
+}
+
+startApolloServer();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+});
