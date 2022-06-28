05a2ffe356f2cb0130f882b19ee79f83bcbf3142
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-06-28 09:10:53 -0700

    add basic user-routes

---
+++ server/routes/api/user-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+const {
+  createUser,
+  getSingleUser,
+  login,
+} = require('../../controllers/user-controller');
+
+// import middleware
+const { authMiddleware } = require('../../utils/auth');
+
+// put authMiddleware anywhere we need to send a token for verification of user
+router.route('/').post(createUser).put(authMiddleware, savedNfts);
+
+router.route('/login').post(login);
+
+module.exports = router;
