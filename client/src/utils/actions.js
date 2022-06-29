3d7440ae588d5aef9ca2b90b851a5b98cdf3e761
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-06-28 19:20:25 -0700

    add product component and actions utility

---
+++ client/src/utils/actions.js
@@ -0,0 +1,17 @@
+// update exports for products, categories
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+
+
+// Everything Cart related:
+export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
+
+export const ADD_TO_CART = "ADD_TO_CART";
+export const ADD_MULTIPLE_TO_CART = "ADD_MULTIPLE_TO_CART";
+
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+export const CLEAR_CART = "CLEAR_CART";
+
+export const TOGGLE_CART = "TOGGLE_CART";
