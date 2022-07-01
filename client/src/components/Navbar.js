918e60b8e486cbf3a351f14b54f3280bab5e36b6
Travis Nelson <96236628+ValiantThor92@users.noreply.github.com>
2022-07-01 10:46:14 -0700

    add style classes for boxes but dont work yet, add filler mainpage background gif

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { Navbar, Nav, Container, Modal, Tab } from "react-bootstrap";
 import SignUpForm from "./SignupForm";
 import LoginForm from './LoginForm';
-
 import Auth from "../utils/auth";
 
 const AppNavbar = () => {
